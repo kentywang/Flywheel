c76ae2a3e8094ecdeaaa17c6308484c6a6f3b955
Kenty Wang <kentymail@gmail.com>
2017-10-11 12:34:48 -0400

    lint

--- controller.js
+++ controller.js
@@ -1,160 +1,135 @@
 const tabDistFromCenterMultiplier = 20;
 
-// let canAddHud = true;
+let canAddHud = true;
 
-function onKeyDown (e) {
-	// console.log('KEYDOWN')
-	if (e.key === "Alt") {
-		chrome.runtime.sendMessage({action: "keyDown"}, response => {
-			// console.log('addtopage from keydown resp')
-			addToPage(response.payload);
-		});
-	}
-}
+function addToPage({ flywheel, activeTabIndex, selectedTabIndex }) {
+  if (canAddHud) {
+    cleanUp();
+    canAddHud = false;
 
-function onKeyUp (e) {
-	// console.log('KEYUP')
-	if (e.key === "Alt") {
-		console.log('cleanup from keyup')
-		cleanUp();
-	}
-}
+    new Promise((resolve) => {
+      resolve(window.addEventListener('keyup', onKeyUp));
+    })
+      .then(() => (
+        document.addEventListener('mousemove', updatePosition)
+      ))
+      .then(() => (
+        document.addEventListener('webkitvisibilitychange', handleVisibilityChange)
+      ))
+      .then(() => {
+        console.log('keyup prob attached');
 
-function updatePosition (e) {
-	if (!e.altKey) {
-		// this is a bandage solution to problem with the rare keyup being lost 
-		// in between tab switches
-		cleanUp();
-	} else {
-		chrome.runtime.sendMessage({
-			action: "mouseMoved",
-			payload: {x: e.movementX, y: e.movementY}
-		});
-	}
-}
+        // create ordered list of tabs
+        const hud = document.createElement('div');
+        hud.setAttribute('id', 'hud');
 
-function addToPage ({flywheel, activeTabIndex, selectedTabIndex}) {
-	// if (canAddHud) {
-		cleanUp();
-		// create ordered list of tabs
-		const hud = document.createElement("div");
-		hud.setAttribute("id", "hud");
+        const ring = document.createElement('ol');
+        hud.appendChild(ring);
 
-		const ring = document.createElement("ol")
-		hud.appendChild(ring);
+        flywheel.forEach((tab, i) => {
+          // create item in list
+          const item = document.createElement('li');
 
-		flywheel.forEach((tab, i) => {
-			// create item in list
-			const item = document.createElement("li");
+          // amplify coords and stringify with relevant CSS units
+          item.style.marginTop = `${tab.x * tabDistFromCenterMultiplier}vw`;
+          item.style.marginLeft = `${tab.y * tabDistFromCenterMultiplier}vw`;
 
-			// amplify coords and stringify with relevant CSS units
-			item.style.marginTop = tab.x * tabDistFromCenterMultiplier + "vw";
-			item.style.marginLeft = tab.y * tabDistFromCenterMultiplier + "vw";
+          // add title
+          item.appendChild(document.createTextNode(tab.title));
 
-			// add title
-			item.appendChild(document.createTextNode(tab.title));
-			
-			// add favicon
-			const image = document.createElement("img");
-			image.src = tab.favIconUrl;
-			image.width = "32";
-			image.height = "32";
-			item.appendChild(image);
+          // add favicon
+          const image = document.createElement('img');
+          image.src = tab.favIconUrl;
+          image.width = '32';
+          image.height = '32';
+          item.appendChild(image);
 
-			// highlight selected tab
-			if (i === selectedTabIndex) {
-				item.classList.add("selected");
-			}
+          // highlight selected tab
+          if (i === selectedTabIndex) {
+            item.classList.add('selected');
+          }
 
-			if (i === activeTabIndex) {
-				item.classList.add("active");
-			}
+          if (i === activeTabIndex) {
+            item.classList.add('active');
+          }
 
-			// add item to list
-			ring.appendChild(item);
-		});
+          // add item to list
+          ring.appendChild(item);
+        });
 
-		// add list to doc body
-		document.body.appendChild(hud); 
-
-		document.addEventListener("mousemove", updatePosition);
-		document.addEventListener("webkitvisibilitychange", handleVisibilityChange);
-
-		window.addEventListener("keyup", onKeyUp);
-		
-		// canAddHud = false;
-	// }
+        // add list to doc body
+        document.body.appendChild(hud);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
 
 function handleVisibilityChange() {
-	if (document.webkitHidden) {
-		console.log('cleanup from tabhide')
-		cleanUp();
-	}
+  if (document.webkitHidden) {
+    console.log('cleanup from tabhide');
+    cleanUp();
+  }
 }
 
-function cleanUp () {
-	if (document.getElementById("hud")) {
-		document.getElementById("hud").remove();
-		document.removeEventListener("mousemove", updatePosition);
-		document.removeEventListener("webkitvisibilitychange", handleVisibilityChange);
+function cleanUp() {
+  if (document.getElementById('hud')) {
+    document.getElementById('hud').remove();
 
-		window.removeEventListener("keyup", onKeyUp);
-	}
+    document.removeEventListener('mousemove', updatePosition);
+    document.removeEventListener('webkitvisibilitychange', handleVisibilityChange);
 
-	// promisify this?
-	// canAddHud = true;
+    window.removeEventListener('keyup', onKeyUp);
+  }
+
+  // promisify this?
+  canAddHud = true;
+}
+
+function onKeyDown(e) {
+  // console.log('KEYDOWN')
+  if (e.key === 'Alt') {
+    chrome.runtime.sendMessage({ action: 'keyDown' }, (response) => {
+      // console.log('addtopage from keydown resp')
+      addToPage(response.payload);
+    });
+  }
+}
+
+function onKeyUp(e) {
+  console.log('KEYUP');
+  if (e.key === 'Alt') {
+    console.log('cleanup from keyup');
+    cleanUp();
+  }
+}
+
+function updatePosition(e) {
+  if (!e.altKey) {
+    // this is a bandage solution to problem with the rare keyup being lost
+    // in between tab switches
+    cleanUp();
+  } else {
+    chrome.runtime.sendMessage({
+      action: 'mouseMoved',
+      payload: { x: e.movementX, y: e.movementY },
+    });
+  }
 }
 
 // if (window == top) {
-	window.addEventListener("keydown", onKeyDown);
+window.addEventListener('keydown', onKeyDown);
 // }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	switch (request.command) {
-	case "showHud":
-		console.log('addtopage from updatepos resp')
-		addToPage(request.payload);
+chrome.runtime.onMessage.addListener((request) => {
+  switch (request.command) {
+    case 'showHud':
+      console.log('addtopage from updatepos resp');
+      addToPage(request.payload);
 
-		break;
-	default:
-		break;
-	}
+      break;
+    default:
+      break;
+  }
 });
-
-// tasks:
-
-// fix leftover hud [DONE]
-// leftover mouselistener [DONE]
-// errors [DONE]
-// get pointerlock working [DONE]
-// get better mousetracking [DONE]
-// switching to incorrect tabs on input [DONE]
-	// reason: divvying up sections wrong.
-// I think I solved the lefthover hud issue (not the most elegant solution, [DONE]
-	// using state instaed of figuring out root)
-// sticky hud, unsticks after keydown and keyup [DONE]
-	// (my guess is keyup not being detected)
-// page sometimes not detecting any key events until mouseclick [DONE]
-	// think it's cuz focus is on console
-// radial distance from first hold [DONE]
-
-// placeholder favicon
-	// give icons nice border radius!
-
-// normalize css
-// better styling
-
-// handle multiple windows
-
-// namespace #hud
-
-// merge state and pointer
-
-// hud color/bold issues
-
-// selectedtabindex wrong values??
-
-// implement buffer zone for selected tab?
-
-// snapping/slighty-sticky tabs?
--- model.js
+++ model.js
@@ -1,215 +1,220 @@
 const state = {
-	flywheel: [],
-	activeTabIndex: null,
-	selectedTabIndex: null
+  flywheel: [],
+  activeTabIndex: null,
+  selectedTabIndex: null,
 };
 
 const mouseSensitivityThreshold = 50;
 
 class Pointer {
-	constructor (sensitivity) {
-		this.sensitivity = sensitivity;
+  constructor(sensitivity) {
+    this.sensitivity = sensitivity;
 
-		this.x = 0;
-		this.y = 0;
+    this.x = 0;
+    this.y = 0;
 
-		this.hypot = 0;
-		this.radiansAtMouse = null;
-	}
+    this.hypot = 0;
+    this.radiansAtMouse = null;
+  }
 
-	updateCoords ({x, y}) {
-		this.x += x;
-		this.y += y;
+  updateCoords({ x, y }) {
+    this.x += x;
+    this.y += y;
 
-		this.updateDerivs();
-	}
+    this.updateDerivs();
+  }
 
-	resetCoords (x = 0, y = 0) {
-		this.x = x;
-		this.y = y;
+  resetCoords(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
 
-		this.updateDerivs();
-	}
+    this.updateDerivs();
+  }
 
-	updateDerivs() {
-		this.hypot = Math.hypot(this.x, this.y);
+  updateDerivs() {
+    this.hypot = Math.hypot(this.x, this.y);
 
-		// swap arg order for atan2 to rotate 90 degrees
-		// negate y because movement's positive y is down
-		this.radiansAtMouse = Math.atan2(this.x, -this.y);
-	}
+    // swap arg order for atan2 to rotate 90 degrees
+    // negate y because movement's positive y is down
+    this.radiansAtMouse = Math.atan2(this.x, -this.y);
+  }
 
-	overThreshold () {
-		// console.log(this.hypot);
+  overThreshold() {
+    // console.log(this.hypot);
 
-		if (this.hypot > this.sensitivity) {
-			this.pullBackCoords();
-			return true;
-		}
-	}
+    if (this.hypot > this.sensitivity) {
+      this.pullBackCoords();
+      return true;
+    }
+    return false;
+  }
 
-	pullBackCoords () {
-		const ratio = this.hypot / this.sensitivity;
-		
-		this.resetCoords(this.x / ratio, this.y / ratio);
-	}
+  pullBackCoords() {
+    const ratio = this.hypot / this.sensitivity;
+
+    this.resetCoords(this.x / ratio, this.y / ratio);
+  }
 }
 
 const pointer = new Pointer(mouseSensitivityThreshold);
 
-function getTabList () {
-	return new Promise((resolve, reject) => {
-		chrome.windows.getLastFocused({populate: true}, window => {
-			resolve(window.tabs);
-		});
-	});
+function getTabList() {
+  return new Promise((resolve) => {
+    chrome.windows.getLastFocused({ populate: true }, (window) => {
+      resolve(window.tabs);
+    });
+  });
 }
 
-function switchToTabAt (tabList, index) {
-	return new Promise((resolve, reject) => {
-		chrome.tabs.update(tabList[index].id, {active: true}, tab => {
-			resolve(tab);
-		});
-	});
+function switchToTabAt(tabList, index) {
+  return new Promise((resolve) => {
+    chrome.tabs.update(tabList[index].id, { active: true }, (tab) => {
+      resolve(tab);
+    });
+  });
 }
 
 // returns argument list of tabs with coords added
-function withCoords (tabList) {
-	return tabList.map((tab, i) => {
-		const tabWithCoords = Object.assign({}, tab);
+function withCoords(tabList) {
+  return tabList.map((tab, i) => {
+    const tabWithCoords = Object.assign({}, tab);
 
-		// calculate where item will fall on ring menu in radians
-		let radiansAtTab = (tabList.length - i) * (2 * Math.PI) / tabList.length;
+    // calculate where item will fall on ring menu in radians
+    let radiansAtTab = ((tabList.length - i) * (2 * Math.PI)) / tabList.length;
 
-		// rotate 180 degrees to put first item on top
-		radiansAtTab -= Math.PI;
+    // rotate 180 degrees to put first item on top
+    radiansAtTab -= Math.PI;
 
-		// get cartesian coords from radians
-		tabWithCoords.x = Math.cos(radiansAtTab);
-		tabWithCoords.y = Math.sin(radiansAtTab);
+    // get cartesian coords from radians
+    tabWithCoords.x = Math.cos(radiansAtTab);
+    tabWithCoords.y = Math.sin(radiansAtTab);
 
-		return tabWithCoords;
-	});
+    return tabWithCoords;
+  });
 }
 
-function determineSelectedTabIndex (radiansAtMouse, tabListLength) {
-	const radiansPerTab = (2 * Math.PI) / tabListLength;
+function determineSelectedTabIndex(radiansAtMouse, tabListLength) {
+  const radiansPerTab = (2 * Math.PI) / tabListLength;
 
-	// offset to rotate tracking clockwise half a tab unit
-	const offset = radiansPerTab / 2;
+  // offset to rotate tracking clockwise half a tab unit
+  const offset = radiansPerTab / 2;
 
-	// convert negative radians to positive
-	if (radiansAtMouse < 0) {
-		radiansAtMouse += (2 * Math.PI);
-	}
+  // convert negative radians to positive
+  if (radiansAtMouse < 0) {
+    radiansAtMouse += (2 * Math.PI);
+  }
 
-	// console.log(radiansAtMouse, radiansPerTab, Math.floor((radiansAtMouse + offset) / radiansPerTab) % tabListLength);
+  // console.log(
+  //   radiansAtMouse,
+  //   radiansPerTab,
+  //   Math.floor((radiansAtMouse + offset) / radiansPerTab) % tabListLength
+  // );
 
-	// need modulo because the radians with offset can exceed 2π
-	return Math.floor((radiansAtMouse + offset) / radiansPerTab) % tabListLength;
+  // need modulo because the radians with offset can exceed 2π
+  return Math.floor((radiansAtMouse + offset) / radiansPerTab) % tabListLength;
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	switch (request.action) {
-	case "keyDown":
-		// console.log('keyDown')
-		getTabList()
-		.then(tabList => {
-			// store culled property list with coords
-			state.flywheel = withCoords(tabList.map(({
-				title,
-				favIconUrl,
-				id,
-				active
-			}) => ({
-				title,
-				favIconUrl,
-				id,
-				active
-			})));
+  switch (request.action) {
+    case 'keyDown':
+      // console.log('keyDown')
+      getTabList()
+        .then((tabList) => {
+          // store culled property list with coords
+          state.flywheel = withCoords(tabList.map(({
+            title,
+            favIconUrl,
+            id,
+            active,
+          }) => ({
+            title,
+            favIconUrl,
+            id,
+            active,
+          })));
 
-			// store index of active tab
-			state.activeTabIndex = state.flywheel.findIndex(tab => (
-				tab.active
-			));
+          // store index of active tab
+          state.activeTabIndex = state.flywheel.findIndex(tab => (
+            tab.active
+          ));
 
-			state.selectedTabIndex = null;
+          state.selectedTabIndex = null;
 
-			// console.log(state)
+          // console.log(state)
 
-			sendResponse({
-				command: "showTabs",
-				payload: state
-			});
+          sendResponse({
+            command: 'showTabs',
+            payload: state,
+          });
 
-			pointer.resetCoords();
-		});
+          pointer.resetCoords();
+        });
 
-		break;
-	case "mouseMoved":
-		pointer.updateCoords(request.payload);
+      break;
+    case 'mouseMoved':
+      pointer.updateCoords(request.payload);
 
-		state.selectedTabIndex = pointer.overThreshold() ?
-			determineSelectedTabIndex(
-				pointer.radiansAtMouse,
-				state.flywheel.length
-			) :
-			null;
+      state.selectedTabIndex = pointer.overThreshold() ?
+        determineSelectedTabIndex(
+          pointer.radiansAtMouse,
+          state.flywheel.length,
+        ) :
+        null;
 
-		new Promise((resolve, reject) => {
-			// check if the newly selected tab is different from active tab
-			if (
-				state.flywheel[state.selectedTabIndex]
-				&& !state.flywheel[state.selectedTabIndex].active
-			) {
-				// console.log(`from ${sender.url}, switch to`, state.selectedTabIndex, Date.now())
-				resolve(switchToTabAt(state.flywheel, state.selectedTabIndex));
-			} else {
-				resolve(true);
-			}
-		})
-		.then(() => {
-			return getTabList();
-		})
-		.then(tabList => {
-			// update tabList again since active tab may have changed
-			state.flywheel = withCoords(tabList.map(({
-				title,
-				favIconUrl,
-				id,
-				active
-			}) => ({
-				title,
-				favIconUrl,
-				id,
-				active
-			})));
+      new Promise((resolve) => {
+        // check if the newly selected tab is different from active tab
+        if (
+          state.flywheel[state.selectedTabIndex]
+          && !state.flywheel[state.selectedTabIndex].active
+        ) {
+          // console.log(`from ${sender.url}, switch to`, state.selectedTabIndex, Date.now())
+          resolve(switchToTabAt(state.flywheel, state.selectedTabIndex));
+        } else {
+          resolve(true);
+        }
+      })
+        .then(() => (
+          getTabList()
+        ))
+        .then((tabList) => {
+          // update tabList again since active tab may have changed
+          state.flywheel = withCoords(tabList.map(({
+            title,
+            favIconUrl,
+            id,
+            active,
+          }) => ({
+            title,
+            favIconUrl,
+            id,
+            active,
+          })));
 
-			// console.log(state.selectedTabIndex, state.flywheel.length)
+          // console.log(state.selectedTabIndex, state.flywheel.length)
 
-			state.activeTabIndex = state.flywheel.findIndex(tab => (
-				tab.active
-			));
+          state.activeTabIndex = state.flywheel.findIndex(tab => (
+            tab.active
+          ));
 
-			// pass state to newly activated tab
-			chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					command: "showHud",
-					payload: state
-				});
-			});
-			// maybe I can start preloading the flywheel to the new active tab
-			// before it is switched to
-		})
-		.catch(error => {
-			console.log(error);
-		});
+          // pass state to newly activated tab
+          chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, {
+              command: 'showHud',
+              payload: state,
+            });
+          });
+          // maybe I can start preloading the flywheel to the new active tab
+          // before it is switched to
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
-		break;
-	default:
-		break;
-	}
+      break;
+    default:
+      break;
+  }
 
-	// need this to send async response
-	return true;
+  // need this to send async response
+  return true;
 });
